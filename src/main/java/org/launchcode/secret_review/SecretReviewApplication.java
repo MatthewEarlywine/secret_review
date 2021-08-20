3f61f6d8f211bb7452fba94c4105ed74a495b921
mohamed <elkahzen1@gmail.com>
2021-08-19 21:06:26 -0500

    initial commit

---
+++ src/main/java/org/launchcode/secret_review/SecretReviewApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.secret_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretReviewApplication.class, args);
+	}
+
+}
